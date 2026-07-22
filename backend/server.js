960130af601035ecf9a8360614984ecb8f411c99
Paulo Ricardo Miguez de Oliveira <98716515+PauloMiguez@users.noreply.github.com>
2026-07-22 20:40:42 -0300

    Complete admin.js with upload functions

--- backend/server.js
+++ backend/server.js
@@ -481,14 +481,8 @@ app.get('/admin', (req, res) => {
 // ============================================================
 //  CONFIGURAÇÃO CLOUDINARY
 // ============================================================
-const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 // Configuração do Multer para upload de arquivos em memória
 const storage = multer.memoryStorage();
